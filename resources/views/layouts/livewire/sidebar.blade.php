7bd911d9c32d040669bd31ccbee66200438bde47
Pentesting-28 <mpenav28@gmail.com>
2026-02-21 20:41:19 -0400

    feat: add Livewire sidebar component with dashboard, authors, and books navigation.

--- resources/views/layouts/livewire/sidebar.blade.php
+++ resources/views/layouts/livewire/sidebar.blade.php
@@ -1,25 +1,28 @@
 <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
     <flux:sidebar.header>
-        <x-app-logo :sidebar="true" name_mode="Livewire" href="{{ route('authors.index') }}" wire:navigate />
+        <x-app-logo :sidebar="true" name_mode="Livewire" href="{{ route('livewire.dashboard') }}" wire:navigate />
         <flux:sidebar.collapse class="lg:hidden" />
     </flux:sidebar.header>
 
     <flux:sidebar.nav>
         <flux:sidebar.group :heading="__('Platform')" class="grid">
-            <flux:sidebar.item icon="squares-2x2" :href="route('authors.index')" :current="request()->is('authors*')" wire:navigate>
+            <flux:sidebar.item icon="squares-2x2" :href="route('livewire.dashboard')" :current="request()->is('authors*')" wire:navigate>
                 {{ __('Dashboard') }}
             </flux:sidebar.item>
         </flux:sidebar.group>
 
         <flux:sidebar.group :heading="__('Módulos')" class="grid">
-            <flux:sidebar.item icon="users" :href="route('authors.index')" :current="request()->routeIs('authors.*')" wire:navigate>
-                {{ __('Autores') }}
-                <flux:badge size="sm" color="purple" inset="top bottom" class="ml-auto">LW</flux:badge>
-            </flux:sidebar.item>
+            <flux:navlist>
+                <flux:navlist.group expandable :heading="__('Biblioteca')" icon="building-library" :expanded="request()->routeIs('authors.*')">
+                    <flux:navlist.item icon="users" :href="route('authors.index')" :current="request()->routeIs('authors.*')" wire:navigate>
+                        {{ __('Autores') }}
+                    </flux:navlist.item>
 
-            <flux:sidebar.item icon="book-open" href="#" class="opacity-50">
-                {{ __('Libros') }}
-            </flux:sidebar.item>
+                    <flux:navlist.item icon="book-open" href="#" class="opacity-50" wire:navigate>
+                        {{ __('Libros') }}
+                    </flux:navlist.item>
+                </flux:navlist.group>
+            </flux:navlist>
         </flux:sidebar.group>
     </flux:sidebar.nav>
 
